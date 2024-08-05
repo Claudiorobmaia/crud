c6b6892a27fd244eb5e4145b0b7faab94b3709df
Claudio Maia <claudiorm3@hotmail.com>
2024-08-05 15:11:06 -0300

    vercel file

--- src/models/produtos.js
+++ src/models/produtos.js
@@ -4,7 +4,9 @@ const produtoSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     nome: { type: String, require: true },
     preco: { type: Number },
-    descricao: { type: String }
+    descricao: { type: String },
+    imagem: { type: String }
+
 }, { versionKey: false });
 
 const produto = mongoose.model("produtos", produtoSchema);
