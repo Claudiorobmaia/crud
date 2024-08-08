e32fe2afb78ef5cfbeaaf20f4fd76d75d3b61828
Claudio Maia <claudiorm3@hotmail.com>
2024-08-08 11:25:04 -0300

    vercel

--- api/config/dbConnect.js
+++ api/config/dbConnect.js
@@ -1,11 +1,33 @@
-import mongoose, { mongo } from "mongoose";
+// import mongoose, { mongo } from "mongoose";
+
+// async function conectaNaDatabase() {
+//     mongoose.connect(process.env.MONGODB_URI);
+//     return mongoose.connection;
+// };
+
+// export default conectaNaDatabase;
+
+
+import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect(process.env.MONGODB_URI);
-    return mongoose.connection;
+    try {
+        if (mongoose.connection.readyState === 0) { // Verifica se já não está conectado
+            await mongoose.connect(process.env.MONGODB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                // Adicione outras opções aqui, se necessário
+            });
+            console.log("Conectado ao MongoDB");
+        } else {
+            console.log("Já conectado ao MongoDB");
+        }
+        return mongoose.connection;
+    } catch (error) {
+        console.error("Erro ao conectar no MongoDB:", error);
+        throw error; // Rejeita a promessa com o erro
+    }
 };
 
 export default conectaNaDatabase;
-
-
 
